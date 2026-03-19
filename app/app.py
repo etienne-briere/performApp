80a0afe0093887c6f25a85d29bfd4b23754b80fa
etienne-briere <etienne.briere42@gmail.com>
2026-03-19 19:38:08 +0100

    update_folders

--- app/app.py
+++ app/app.py
@@ -15,10 +15,6 @@ from app.logic.profile_logic import ProfileController
 from app.core.sync_service import SyncService
 from app.widgets.folder_animation import FolderAnimation
 
-from app.screens.view_screen import ViewScreen
-from app.screens.record_screen import RecordScreen
-from app.screens.history_screen import HistoryScreen
-
 # Logger
 from utils.logger import get_logger
 
@@ -33,6 +29,10 @@ class PerformApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        # Initialisation des attributs de l'application
+        self.profile = None
+        self.sync = None
+
         logger.info("Initialisation de l'application KCApp")
 
     def build(self):
@@ -42,7 +42,8 @@ class PerformApp(MDApp):
         logger.info("Construction de l'interface...")
 
         # Initialiser les gestionnaires
-
+        self.profile = ProfileController(app=self)
+        self.sync = SyncService(self)
 
         # Définir le thème de l'application
         self.theme_cls.theme_style = THEME_STYLE
@@ -65,6 +66,15 @@ class PerformApp(MDApp):
             self.icon_size = sp(25)
             self.smiley_icon_size = sp(60)
 
+        # Charger les fichiers .kv
+        Builder.load_file("ui/kv/view_screen.kv")
+        Builder.load_file("ui/kv/history_screen.kv")
+        Builder.load_file("ui/kv/login_dialog.kv")
+        Builder.load_file("ui/kv/settings_screen.kv")
+        Builder.load_file("ui/kv/record_screen.kv")
+        Builder.load_file("ui/kv/home_screen.kv")
+        Builder.load_file("ui/kv/main.kv")
+
         # Données smileys
         self.smiley_data = [
             ("emoticon-dead-outline", (1, 0, 0, 1)),  # Rouge
@@ -79,11 +89,6 @@ class PerformApp(MDApp):
         self.today = datetime.today()
         self.today_str = datetime.today().strftime("%d/%m/%Y")
 
-        # Instances vers les autres class Python
-        self.profile = ProfileController(app=self)
-
-        self.sync = SyncService(self)
-
         return Builder.load_file("ui/main.kv")
 
     def on_start(self):
@@ -95,13 +100,13 @@ class PerformApp(MDApp):
         # ScreenManager
         self.sm = self.root.ids.screen_manager
 
-        # Vraies instances créées par le chargement des fichiers .kv
-        self.view = self.root.ids.screen_manager.get_screen("view")
-        self.view.app = self
-        self.record = self.root.ids.screen_manager.get_screen("record")
-        self.record.app = self
-        self.history = self.root.ids.screen_manager.get_screen("history")
-        self.history.app = self
+        # # Vraies instances créées par le chargement des fichiers .kv
+        # self.view = self.root.ids.screen_manager.get_screen("view")
+        # self.view.app = self
+        # self.record = self.root.ids.screen_manager.get_screen("record")
+        # self.record.app = self
+        # self.history = self.root.ids.screen_manager.get_screen("history")
+        # self.history.app = self
 
     def on_stop(self):
         """
